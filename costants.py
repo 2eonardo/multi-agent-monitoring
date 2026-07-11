a8ccbef5db0f98c61bfcc3d04c73abfd745acada
2eonardo <leonardo.soldani3@edu.unifi.it>
2026-07-11 18:09:07 +0200

    Prima implementazioni costanti

---
+++ costants.py
@@ -0,0 +1,16 @@
+# constants.py
+
+# map parameters
+DECAY_RATE = 0.95
+
+# agent parameters
+NUM_AGENTS = 5
+SENSOR_RANGE = 2 # in grid cells
+MAX_DISPLACEMENT = 3 #range of max step
+
+# Start position of the agents (row, col)
+START_ROW = 100
+START_COL = 150
+
+# Random start position of the agents (row, col)
+RANDOM_SPAWN = True
