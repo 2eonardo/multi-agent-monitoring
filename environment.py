2d60e280dddfbadbd7cb45d222199c41e169db0b
2eonardo <leonardo.soldani3@edu.unifi.it>
2026-07-11 15:02:27 +0200

    Aggiornamento raggio e funzione

--- environment.py
+++ environment.py
@@ -12,6 +12,13 @@ class Map:
         #grid of monitoring value
         self.shape = self.land_mask.shape
         self.grid = np.zeros(self.shape)
+        #function to be maximized
+        self.function_value = 0.0
+
+    def update_function_value(self):
+        # Update the function value based on the current grid state
+        self.function_value = float(np.sum(self.grid[self.sea_mask]))
+        return self.function_value
 
     # check if cell belongs to the sea
     def is_sea(self, x, y):
@@ -21,11 +28,19 @@ class Map:
         return False
 
     # Set cell value to 1 when visited by a robot.
-    def cell_view(self, x, y):
-        if self.is_sea(x, y):
-            self.grid[x, y] = 1.0
-            return True
-        return False
+    def cell_view(self, x, y, radius):
+        if radius == 0:
+            if self.is_sea(x, y):
+                self.grid[x, y] = 1.0
+                return True
+            return False
+        any_cell_updated = False
+        for r in range(x -radius,x + radius + 1):
+            for c in range(y -radius,y + radius + 1):
+                if self.is_sea(r, c):
+                    self.grid[r, c] = 1.0
+                    any_cell_updated = True
+        return any_cell_updated
 
     # decay function
     def decay(self, decay_rate):
