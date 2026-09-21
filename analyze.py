efc9dc2c99c09a51d7a12fe2d68c0246daa5d2de
2eonardo <leonardo.soldani3@edu.unifi.it>
2026-09-21 14:38:27 +0200

    Risultati testing

--- analyze.py
+++ analyze.py
@@ -9,7 +9,7 @@ import costants as c
 
 
 def main():
-    test_tag = f"_spawn_radius_100"
+    test_tag = f""
     fps_video = 24
     repository = f"results{test_tag}"
     file_media = f"{repository}/data/media_data"
